bf9ed095b531c38a9e2f30f4deead36c1761c144
Bereket Sega <bereketsega8@gmail.com>
2023-02-16 19:06:08 -0500

    experience update

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React from 'react';
 
 const Experience = () => {
-  return (
-    <div className="experience">
-      <div className="cmd">$ whois -u bereket --experience</div>
-      <div className="cout">i tutor college students in math at towson university.</div>
-    </div>
-  );
+	return (
+		<div className='experience'>
+			<div className='cmd'>$ whois -u bereket --experience</div>
+			<div className='cout'>
+				i work as a software developer intern at <a href='https://secured.team'><i>secured inc. </i></a>
+				and tutor at <a href='https://www.towson.edu'><i>towson university</i></a>.
+			</div>
+		</div>
+	);
 };
 
 export default Experience;
