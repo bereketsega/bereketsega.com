af08f99ae7b94a9e57620937a3d1c2e24831d00b
Bereket Sega <bereketsega8@gmail.com>
2023-03-02 18:56:15 -0500

    wording

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -5,7 +5,7 @@ const Experience = () => {
 		<div className='experience'>
 			<div className='cmd'>$ whois -u bereket --experience</div>
 			<div className='cout'>
-				i work as a software developer intern at <a href='https://secured.team' target={'_blank'}><i>secured inc. </i></a>
+				i'm a software developer intern at <a href='https://secured.team' target={'_blank'}><i>secured inc. </i></a>
 				and tutor at <a href='https://www.towson.edu' target={'_blank'}><i>towson university</i></a>.
 			</div>
 		</div>
